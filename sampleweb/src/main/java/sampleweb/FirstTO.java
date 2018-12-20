5b522824ca0877b5d3fd8c501d24e7cacfcc8d6a
i320626 <biplob.ghosh@sap.com>
2018-12-20 23:52:21 +0530

    sample web app with jax rs
    Change-Id: Iad67ca8237f1a7581038048b92268abe53988124

---
+++ sampleweb/src/main/java/sampleweb/FirstTO.java
@@ -0,0 +1,28 @@
+package sampleweb;
+
+public class FirstTO {
+	
+	public FirstTO(String str, int num) {
+		super();
+		this.str = str;
+		this.num = num;
+	}
+	private String str;
+	private int num;
+	
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+
+}
